131087445880f7d830c217ac442aaa019b645c5e
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-23 20:37:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_8_Constructors_ConsoleApp
+{
+    class Book
+    { 
+    public string title;
+    public string author;
+    public int pages;
+
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+    
+        
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            Book book2 = new Book("Lord of the Rings", "Talkein", 900);
+
+            Console.WriteLine("The title of book1 is " + book1.title);
+            Console.WriteLine("The number of pages of book2 is " + book2.pages);
+
+            Console.ReadLine();
+        }
+    }
+}
